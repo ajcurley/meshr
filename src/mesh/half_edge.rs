b0eaa2217175c118de2f2e51574256dcf9944c8f
Alex Curley <=>
2024-11-14 16:21:05 -0500

    Outline HeMesh methods

--- src/mesh/half_edge.rs
+++ src/mesh/half_edge.rs
@@ -1,4 +1,5 @@
 use crate::geometry::Vector3;
+use crate::mesh::{ObjReader, PolygonSoupMesh};
 
 #[derive(Debug, Clone)]
 pub struct HeMesh {
@@ -9,9 +10,16 @@ pub struct HeMesh {
 }
 
 impl HeMesh {
+    /// Construct a half edge mesh from a polygon soup mesh
+    pub fn from_polygon_soup(soup: &PolygonSoupMesh) -> HeMesh {
+        unimplemented!();
+    }
+
     /// Import a half edge mesh from an OBJ file
     pub fn import_obj(path: &str) -> std::io::Result<HeMesh> {
-        unimplemented!();
+        let soup = ObjReader::new(&path).read()?;
+        let mesh = HeMesh::from_polygon_soup(&soup);
+        Ok(mesh)
     }
 
     /// Export a half edge mesh to an OBJ file
@@ -51,6 +59,36 @@ impl HeMesh {
             .filter(|h| !h.is_boundary())
             .find(|h| self.half_edges[h.twin.unwrap()].origin == h.origin)
             .is_none()
+    }
+
+    /// Get the contiguous faces as components
+    pub fn components(&self) -> Vec<Vec<usize>> {
+        unimplemented!();
+    }
+
+    /// Orient the mesh
+    pub fn orient(&mut self) {
+        unimplemented!();
+    }
+
+    /// Zip any open edges
+    pub fn zip_edges(&mut self) {
+        unimplemented!();
+    }
+
+    /// Merge naively with another mesh
+    pub fn merge(&mut self, other: &HeMesh) {
+        unimplemented!();
+    }
+
+    /// Extract the subset of faces into a new mesh
+    pub fn extract_face(&self, faces: &[usize]) -> HeMesh {
+        unimplemented!();
+    }
+
+    /// Extract the subset of patches by name into a new mesh
+    pub fn extract_patches(&self, patches: &[&str]) -> HeMesh {
+        unimplemented!();
     }
 }
 
