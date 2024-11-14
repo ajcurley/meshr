89d2b77fa9b42bb042ac448ee90b68ebbbd85028
Alex Curley <=>
2024-11-14 17:16:00 -0500

    Initial Python bindings

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,3 @@
 pub mod geometry;
 pub mod mesh;
 pub mod spatial;
-
-// Python bindings
-use pyo3::prelude::*;
-
-mod bindings;
-
-#[pymodule]
-fn meshr(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_class::<bindings::python::geometry::Vector3>()?;
-
-    Ok(())
-}
