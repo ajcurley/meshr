f0698cdaaf6c00b419861a61166d00ca68b21b4c
Alex Curley <=>
2024-11-14 16:44:34 -0500

    Checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
 pub mod geometry;
 pub mod mesh;
 pub mod spatial;
+
+// Python bindings
+use pyo3::prelude::*;
+
+mod bindings;
+
+#[pymodule]
+fn meshr(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_class::<bindings::python::geometry::Vector3>()?;
+
+    Ok(())
+}
